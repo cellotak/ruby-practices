14c78ab0b18cecca70067e38ceacd07896d734f4
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-06 14:59:10 +0900

    ls.rbのrubocopによる修正とリファクタリング

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,7 +7,7 @@ require 'etc'
 MAX_COL_COUNT = 3
 SPACE_WIDTH = 2
 
-FILE_TYPE_LIST = {"01"=>"p", "02"=>"c", "04"=>"d", "06"=>"b", "10"=>"-", "12"=>"l", "14"=>"s"}
+FILE_TYPE_LIST = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
 
 def parse_options(argv)
   opt = OptionParser.new
@@ -26,14 +26,14 @@ def fetch_file_names(target_directory_path, options)
   options[:r] ? file_names.reverse : file_names
 end
 
-def output(target_directory_path, file_names, options)
+def output(file_names, target_directory_path, options)
   return if file_names.empty?
 
   if options[:l]
-    output_long_listing_format(target_directory_path, file_names, options) 
-  else 
-    output_default_format(file_names) 
-  end 
+    output_long_listing_format(file_names, target_directory_path)
+  else
+    output_default_format(file_names)
+  end
 end
 
 def output_default_format(file_names)
@@ -55,48 +55,43 @@ def output_default_format(file_names)
   end
 end
 
-def output_long_listing_format(target_directory_path, file_names, options)
-  file_names.each do |file_name| 
+def output_long_listing_format(file_names, target_directory_path)
+  file_names.each do |file_name|
     stat = File.stat("#{target_directory_path}/#{file_name}")
 
     detail_str =
-      "#{convert_file_mode_to_str(stat)} "\
+      "#{convert_stat_mode_to_str(stat.mode)} "\
       "#{stat.nlink} "\
       "#{Etc.getpwuid(stat.uid).name} "\
       "#{Etc.getgrgid(stat.gid).name} "\
       "#{stat.size} "\
-      "#{stat.ctime.strftime("%b %-d %H:%M")} "\
+      "#{stat.ctime.strftime('%b %-d %H:%M')} "\
       "#{file_name}"
     puts detail_str
   end
 end
 
-def convert_file_mode_to_str(stat)
-  file_type_num = format("%06o",stat.mode).slice(0..1) 
+def convert_stat_mode_to_str(stat_mode)
+  file_type_num = format('%06o', stat_mode).slice(0..1)
   file_type_str = FILE_TYPE_LIST[file_type_num]
 
-  permission_num = format("%06o",stat.mode).slice(3..5)
+  permission_num = format('%06o', stat_mode).slice(3..5)
   permission_str = convert_permission_num_to_str(permission_num)
 
   file_type_str + permission_str
-
 end
 
 def convert_permission_num_to_str(permission_num)
   permission_str_array = []
   permission_num.chars.each do |nc|
     n = nc.to_i
-    permission_array = ["-","-","-"]
-    if n / 4 == 1
-      permission_array[0] = "r"
-    end
-    if (n / 2) % 2 == 1
-      permission_array[1] = "w"
-    end
-    if n % 2 == 1
-      permission_array[2] = "x"
-    end
-    permission_str_array +=  permission_array
+    permission_array = Array.new(3, '-')
+
+    permission_array[0] = 'r' if n / 4 == 1
+    permission_array[1] = 'w' if (n / 2).odd?
+    permission_array[2] = 'x' if n.odd?
+
+    permission_str_array += permission_array
   end
   permission_str_array.join
 end
@@ -105,4 +100,4 @@ end
 options, directory_paths = parse_options(ARGV)
 target_directory_path = directory_paths[0] || './'
 file_names = fetch_file_names(target_directory_path, options)
-output(target_directory_path, file_names, options)
+output(file_names, target_directory_path, options)
