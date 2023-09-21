9e8d68b0cfe55e39cd9690d132f58b15dc75edfa
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-21 13:56:39 +0900

    mainメソッドを修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,7 +15,7 @@ def main
   # directory_pathsには複数のpathを指定することは許容しているが、現時点でファイル名を表示するのは1番目に指定したディレクトリのみにしている。
   directory_path = directory_paths[0] || './'
   file_names = fetch_file_names(directory_path, options)
-  output(file_names, target_directory_path, options)
+  output(file_names, directory_path, options)
 end
 
 def parse_options(argv)
