ef779268ecf350c3158c0da98599dc9db8a88ffb
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-10-03 06:21:06 +0900

    メソッドの記述順を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -45,12 +45,13 @@ def output(file_names, target_directory_path, options)
 end
 
 def output_long_listing_format(file_names, target_directory_path)
-  details_by_file_name = get_details_by_file_name(file_names, target_directory_path)
-
   puts "total #{calc_block_count_total(file_names, target_directory_path)}"
+
+  details_by_file_name = get_details_by_file_name(file_names, target_directory_path)
 
   file_names.each do |file_name|
     details = details_by_file_name[file_name]
+
     details.each_with_index do |detail, index|
       width = get_max_width_by_detail(details_by_file_name, index)
       if /^\d+$/.match?(detail)
@@ -64,6 +65,14 @@ def output_long_listing_format(file_names, target_directory_path)
   end
 end
 
+def calc_block_count_total(file_names, target_directory_path)
+  block_count_total = 0
+  file_names.each do |file_name|
+    block_count_total += File.stat("#{target_directory_path}/#{file_name}").blocks
+  end
+  block_count_total
+end
+
 def get_details_by_file_name(file_names, target_directory_path)
   details_by_file_name = {}
   file_names.each do |file_name|
@@ -74,33 +83,6 @@ def get_details_by_file_name(file_names, target_directory_path)
   details_by_file_name
 end
 
-def calc_block_count_total(file_names, target_directory_path)
-  block_count_total = 0
-  file_names.each do |file_name|
-    block_count_total += File.stat("#{target_directory_path}/#{file_name}").blocks
-  end
-  block_count_total
-end
-
-def output_default_format(file_names)
-  row_count = ((file_names.size - 1) / MAX_COL_COUNT) + 1
-  col_count = [file_names.size, MAX_COL_COUNT].min
-
-  # NOTE: OS標準のlsコマンドは横並びではなく縦並びで出力される(転置して出力される)
-  # NOTE: file_names_tableの要素は行と列が出力したい形(縦並び)とは逆で保存されている
-  file_names_table = file_names.each_slice(row_count).to_a
-  widths = file_names_table.map { |col| col.map(&:size).max + SPACE_WIDTH }
-
-  row_count.times do |row_index|
-    col_count.times do |col_index|
-      # file_names_tableの行と列が逆で保存されているので、col_indexとrow_indexを入れ替えて出力させている
-      target_file_name = file_names_table[col_index][row_index]
-      print target_file_name&.ljust(widths[col_index])
-    end
-    print "\n"
-  end
-end
-
 def convert_stat_to_details(stat)
   details = []
   details << convert_stat_mode_to_str(stat.mode)
@@ -109,11 +91,6 @@ def convert_stat_to_details(stat)
   details << Etc.getgrgid(stat.gid).name
   details << stat.size.to_s
   details << stat.ctime.strftime('%b %e %H:%M')
-end
-
-def get_max_width_by_detail(details_by_file_name, index)
-  detail_widths_by_file_name = details_by_file_name.map { |_file_name, details| details[index].length }
-  detail_widths_by_file_name.max
 end
 
 def convert_stat_mode_to_str(stat_mode)
@@ -138,6 +115,30 @@ def convert_permission_code_to_str(permission_code)
     permission_octet_chars.join
   end
   permission_octets.join
+end
+
+def get_max_width_by_detail(details_by_file_name, index)
+  detail_widths_by_file_name = details_by_file_name.map { |_file_name, details| details[index].length }
+  detail_widths_by_file_name.max
+end
+
+def output_default_format(file_names)
+  row_count = ((file_names.size - 1) / MAX_COL_COUNT) + 1
+  col_count = [file_names.size, MAX_COL_COUNT].min
+
+  # NOTE: OS標準のlsコマンドは横並びではなく縦並びで出力される(転置して出力される)
+  # NOTE: file_names_tableの要素は行と列が出力したい形(縦並び)とは逆で保存されている
+  file_names_table = file_names.each_slice(row_count).to_a
+  widths = file_names_table.map { |col| col.map(&:size).max + SPACE_WIDTH }
+
+  row_count.times do |row_index|
+    col_count.times do |col_index|
+      # file_names_tableの行と列が逆で保存されているので、col_indexとrow_indexを入れ替えて出力させている
+      target_file_name = file_names_table[col_index][row_index]
+      print target_file_name&.ljust(widths[col_index])
+    end
+    print "\n"
+  end
 end
 
 main
