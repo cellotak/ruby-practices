84f84ffdbcb09ed9a7184c8b078284db2fa72488
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-02 15:36:32 +0900

    仮のテストを作成

---
+++ 06.bowling_object/bowling_object_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative './frame'
+require_relative './game'
+require_relative './shot'
+
+class BowlingTest < Minitest::Test
+  def test_frame
+    assert Frame.new
+  end
+
+  def test_game
+    assert Game.new
+  end
+
+  def test_shot
+    assert Shot.new
+  end
+end
