46788d796cb2c12e201773e7b95bb876817d2fc4
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-10 10:15:45 +0900

    shot_test修正

--- 06.bowling_object/test/shot_test.rb
+++ 06.bowling_object/test/shot_test.rb
@@ -10,6 +10,11 @@ class ShotTest < Minitest::Test
     assert_equal 'X', shot.shot_char
   end
 
+  def test_shot_char_when_shot_char_is_not_X
+    shot = Shot.new('1')
+    assert_equal '1', shot.shot_char
+  end
+
   def test_shot_score_when_shot_char_is_X
     shot = Shot.new('X')
     assert_equal 10, shot.shot_score
