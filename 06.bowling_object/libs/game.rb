67f24a6a1d4eed68398b16344eb63d3281c290b7
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-14 18:43:32 +0900

    Gameクラスにupdate_bonusメソッドを追加

--- 06.bowling_object/libs/game.rb
+++ 06.bowling_object/libs/game.rb
@@ -18,13 +18,41 @@ class Game
     else
       current_frame.add_shot(char)
     end
+    update_bonus
   end
 
   def current_frame
-    @frames.last
+    frames.last
   end
 
-  
+  def previous_frame
+    frames[-2]
+  end
+
+  def second_previous_frame
+    frames[-3]
+  end
+
+  def update_bonus
+    if current_frame.no_mark? && current_frame.completed?
+      current_frame.comfirm_bonus(0)
+    end
+
+    if previous_frame&.bonus_pended?
+      if previous_frame.strike?
+        if current_frame.shots.size == 2
+          previous_frame.comfirm_bonus(current_frame.shots.sum(&:shot_score))
+        end
+      end
+      if previous_frame.spare?
+        previous_frame.comfirm_bonus(current_frame.shots[0].shot_score)
+      end
+    end
+
+    if second_previous_frame&.bonus_pended?
+      second_previous_frame.comfirm_bonus(10 + current_frame.shots[0].shot_score)
+    end
 
 
+  end
 end
--- 06.bowling_object/test/game_test.rb
+++ 06.bowling_object/test/game_test.rb
@@ -28,7 +28,58 @@ class GameTest < Minitest::Test
     game.add_shot('X')
     game.add_shot('1')
     assert_equal 10, game.frames[0].shots[0].shot_score
-    assert_equal nil, game.frames[0].shots[1]
+    assert_nil game.frames[0].shots[1]
     assert_equal 1, game.frames[1].shots[0].shot_score
+  end
+
+  def test_current_frame
+    game = Game.new
+    6.times do 
+      game.add_shot('1')
+    end
+    assert_equal 3, game.current_frame.frame_number
+  end
+
+  def test_previous_frame
+    game = Game.new
+    6.times do 
+      game.add_shot('1')
+    end
+    assert_equal 2, game.previous_frame.frame_number
+  end
+
+  def test_second_previous_frame
+    game = Game.new
+    6.times do 
+      game.add_shot('1')
+    end
+    assert_equal 1, game.second_previous_frame.frame_number
+  end
+
+  def test_update_bonus1
+    game = Game.new
+    game.add_shot('1')
+    assert_nil game.frames[0].bonus
+  end
+
+  def test_update_bonus2
+    game = Game.new
+    game.add_shot('1')
+    game.add_shot('2')
+    assert_equal 0, game.frames[0].bonus.bonus_score
+  end
+
+  def test_update_bonus3
+    game = Game.new
+    game.add_shot('X')
+    game.add_shot('X')
+    game.add_shot('6')
+    game.add_shot('4')
+    game.add_shot('2')
+    game.add_shot('3')
+    assert_equal 16, game.frames[0].bonus.bonus_score
+    assert_equal 10, game.frames[1].bonus.bonus_score
+    assert_equal 2, game.frames[2].bonus.bonus_score
+    assert_equal 0, game.frames[3].bonus.bonus_score
   end
 end
