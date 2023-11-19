a92cbfad650adc75d590e886a0e9bef608b561e9
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-20 06:14:04 +0900

    Shotsクラスに定数を追加

--- 06.bowling_object/libs/shot.rb
+++ 06.bowling_object/libs/shot.rb
@@ -3,7 +3,9 @@
 class Shot
   attr_reader :score
 
+  ALL_PIN = 10
+
   def initialize(symbol)
-    @score = symbol == 'X' ? 10 : symbol.to_i
+    @score = symbol == 'X' ? ALL_PIN : symbol.to_i
   end
 end
