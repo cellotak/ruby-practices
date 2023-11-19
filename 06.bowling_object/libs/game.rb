5065138ec659958d5342d79f6cbd3f97c64ae169
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-19 22:06:56 +0900

    要件の動作ができるようにバグ修正

--- 06.bowling_object/libs/game.rb
+++ 06.bowling_object/libs/game.rb
@@ -12,8 +12,7 @@ class Game
 
   def add_shot(char)
     if current_frame.completed?
-      next_frame_number = frames.size + 1
-      frames << Frame.new(next_frame_number)
+      frames << Frame.new(current_frame.frame_number + 1)
     end
     current_frame.add_shot(char)
     update_bonus
@@ -32,13 +31,47 @@ class Game
   end
 
   def update_bonus
-    current_frame.comfirm_bonus(0) if current_frame.no_mark? && current_frame.completed?
+    current_frame.comfirm_bonus(0) if (current_frame.no_mark? && current_frame.completed?) || current_frame.frame_number == 10
 
     if previous_frame&.bonus_pended?
       previous_frame.comfirm_bonus(current_frame.shots.sum(&:shot_score)) if current_frame.shots.size == 2 && previous_frame.strike?
       previous_frame.comfirm_bonus(current_frame.shots[0].shot_score) if previous_frame.spare?
     end
 
-    second_previous_frame.comfirm_bonus(10 + current_frame.shots[0].shot_score) if second_previous_frame&.bonus_pended?
+    if second_previous_frame&.bonus_pended?
+      second_previous_frame.comfirm_bonus(10 + current_frame.shots[0].shot_score) if previous_frame.strike?
+      second_previous_frame.comfirm_bonus(10) if previous_frame.spare?
+    end
+  end
+
+  def check_all_shot
+    all_shots = []
+    frames.each do |frame|
+      one_frame = []
+      frame.shots.each do |shot|
+         one_frame << shot.shot_score
+      end
+      all_shots << one_frame
+    end
+    all_shots
+  end
+
+  def check_all_frame_score
+    all_frame_score = []
+    frames.each do |frame|
+      if frame.frame_score.nil?
+        all_frame_score << "#{frame.frame_number}:hoge"
+      else
+        all_frame_score << "#{frame.frame_number} : #{frame.frame_score}"
+      end
+    end
+    all_frame_score
+  end
+
+  def calc_game_score
+    game_score = frames.sum do |frame|
+      frame.frame_score
+    end
+    game_score
   end
 end
--- 06.bowling_object/libs/main.rb
+++ 06.bowling_object/libs/main.rb
@@ -2,33 +2,22 @@
 
 require_relative './game'
 
-# ARGV[0]: '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
+# def input_all_shots(shot_string)
+#   game = Game.new
+#   shot_chars = shot_string.split(',')
+#   shot_chars.each do |shot_char|
+#     game.add_shot(shot_char)
+#   end
+#   game.calc_game_score
+# end
 
-def calc_total_score
-  game = Game.new
-  shot_chars = ARGV[0].split(',')
-  shot_chars.each do |shot_char|
-    game.add_shot(shot_char)
-  end
+
+game = Game.new
+
+shot_string = ARGV[0]
+shot_chars = shot_string.split(',')
+shot_chars.each do |shot_char|
+  game.add_shot(shot_char)
 end
 
-def check_all_shots
-  game = Game.new
-  shot_chars = ARGV[0].split(',')
-  shot_chars.each do |shot_char|
-    game.add_shot(shot_char)
-  end
-
-  all_frames = []
-
-  game.frames.each do |frame|
-    all_shots = []
-    frame.shots.each do |shot|
-      all_shots << shot.shot_score
-    end
-    all_frames << all_shots
-  end
-  all_frames
-end
-
-#=> 139
+puts game.calc_game_score
