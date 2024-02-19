54595a850b8e719a132954cebad94d07674e854f
cellotak <tak.2cellos.ashi.777@gmail.com>
2024-02-19 15:37:22 +0900

    Entryを継承してChildEntryクラスを定義

--- 07.ls_object/test/child_entry_test.rb
+++ 07.ls_object/test/child_entry_test.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require_relative '../libs/child_entry'
 
-puts "child_entry_test"
+class ChildEntryTest < Minitest::Test
+
+  def setup
+    Dir.mkdir('test_directory')
+    File.open('test_directory/hoge1.txt', 'w') {}
+  end
+
+  def teardown
+    File.delete('test_directory/hoge1.txt') if File.exist?('test_directory/hoge1.txt')
+    Dir.rmdir('test_directory') if Dir.exist?('test_directory')
+  end
+
+  def test_child_entry_intialize
+    entry = ChildEntry.new('hoge1.txt', 'test_directory')
+    assert_equal 'hoge1.txt', entry.basename
+    assert_equal 'test_directory', entry.dirname
+    assert_equal 4096, entry.stat.blksize
+    assert_equal 1000, entry.stat.uid
+  end
+end
