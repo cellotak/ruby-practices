60a898906871605ce9527e3a5c5015ebf9579ea2
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-07-11 01:23:54 +0900

    入力部分のテストと実装追加

---
+++ 05.wc/test_wc.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require_relative 'wc'
+
+class TestWc < Minitest::Test
+  def test_parse_options_with_l_option
+    options, file_paths = parse_options(['-l'])
+    assert_equal({ l: true }, options)
+    assert_equal([], file_paths)
+  end
+
+  def test_parse_options_with_w_option
+    options, file_paths = parse_options(['-w'])
+    assert_equal({ w: true }, options)
+    assert_equal([], file_paths)
+  end
+
+  def test_parse_options_with_c_option
+    options, file_paths = parse_options(['-c'])
+    assert_equal({ c: true }, options)
+    assert_equal([], file_paths)
+  end
+
+  def test_parse_options_with_multiple_options
+    options, file_paths = parse_options(['-l', '-w', '-c'])
+    assert_equal({ l: true, w: true, c: true }, options)
+    assert_equal([], file_paths)
+  end
+
+  def test_parse_options_with_file_paths
+    options, file_paths = parse_options(['file1.txt', 'file2.txt'])
+    assert_equal({}, options)
+    assert_equal(['file1.txt', 'file2.txt'], file_paths)
+  end
+
+  def test_parse_options_with_options_and_file_paths
+    options, file_paths = parse_options(['-l', '-w', 'file1.txt', 'file2.txt'])
+    assert_equal({ l: true, w: true }, options)
+    assert_equal(['file1.txt', 'file2.txt'], file_paths)
+  end
+end
---
+++ 05.wc/wc.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+require 'etc'
+
+MAX_COL_COUNT = 3
+SPACE_WIDTH = 2
+
+FILE_TYPE_LIST = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+DETAILS_OUTPUT_ORDER = %i[stat_mode nlink username groupname size ctime].freeze
+RJUST_LIST = %i[nlink size].freeze
+
+def main
+  puts "ARGV: #{ARGV}"
+  p "ARGV: #{ARGV.inspect}"
+  options, file_paths = parse_options(ARGV)
+  # directory_pathsには複数のpathを指定することは許容しているが、現時点でファイル名を表示するのは1番目に指定したディレクトリのみにしている。
+  file_paths.each do |file_path|
+    puts "Processing file: #{file_path}"
+  end
+
+  puts "Options: #{options.inspect}"
+  puts "options[:l] = #{options[:l].inspect}" if options[:l]
+  puts "options[:w] = #{options[:w].inspect}" if options[:w]
+  puts "options[:c] = #{options[:c].inspect}" if options[:c]
+end
+
+def parse_options(argv)
+  opt = OptionParser.new
+  options = {}
+  opt.on('-l') { |v| options[:l] = v }
+  opt.on('-w') { |v| options[:w] = v }
+  opt.on('-c') { |v| options[:c] = v }
+  file_paths = opt.parse(argv)
+  [options, file_paths]
+end
+
+main
