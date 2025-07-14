c6d9e7500d7b60348d5744911ab6b30515ca1048
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-07-14 22:58:30 +0900

    パイプでls-lからファイル名を受け取る処理を追加

--- 05.wc/test_wc.rb
+++ 05.wc/test_wc.rb
@@ -37,4 +37,28 @@ class TestWc < Minitest::Test
     assert_equal({ l: true, w: true }, options)
     assert_equal(['file1.txt', 'file2.txt'], file_paths)
   end
+
+   def test_parse_ls_output
+    ls_output = <<~OUTPUT
+      total 16
+      drwxr-xr-x 2 cellotak cellotak 4096 Jul  9 01:08 .
+      drwxr-xr-x 3 cellotak cellotak 4096 Jul 10 00:50 ..
+      -rw-r--r-- 1 cellotak cellotak    5 Jul  9 01:08 hoge1.txt
+      -rw-r--r-- 1 cellotak cellotak    9 Jul  9 01:07 hoge2.txt
+    OUTPUT
+
+    result = parse_ls_output(ls_output)
+    assert_equal(['hoge1.txt', 'hoge2.txt'], result)
+  end
+
+  def test_parse_ls_output_with_symlink
+    ls_output = <<~OUTPUT
+      total 8
+      -rw-r--r-- 1 user user 5 Jul  9 01:08 file.txt
+      lrwxrwxrwx 1 user user 8 Jul  9 01:08 link.txt -> file.txt
+    OUTPUT
+
+    result = parse_ls_output(ls_output)
+    assert_equal(['file.txt', 'link.txt'], result)
+  end
 end
--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,18 +4,27 @@
 require 'optparse'
 require 'etc'
 
-MAX_COL_COUNT = 3
-SPACE_WIDTH = 2
+# MAX_COL_COUNT = 3
+# SPACE_WIDTH = 2
 
-FILE_TYPE_LIST = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
-DETAILS_OUTPUT_ORDER = %i[stat_mode nlink username groupname size ctime].freeze
-RJUST_LIST = %i[nlink size].freeze
+# FILE_TYPE_LIST = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+# DETAILS_OUTPUT_ORDER = %i[stat_mode nlink username groupname size ctime].freeze
+# RJUST_LIST = %i[nlink size].freeze
 
 def main
   puts "ARGV: #{ARGV}"
   p "ARGV: #{ARGV.inspect}"
   options, file_paths = parse_options(ARGV)
-  # directory_pathsには複数のpathを指定することは許容しているが、現時点でファイル名を表示するのは1番目に指定したディレクトリのみにしている。
+
+  # パイプからの入力を処理
+  if file_paths.empty? && !STDIN.tty?
+    # 標準入力がパイプの場合
+    stdin_content = STDIN.read
+    # 改行で分割してファイルパスのリストを作成
+    # file_paths = stdin_content.strip.split("\n").reject(&:empty?)
+    file_paths = parse_ls_output(stdin_content)
+  end
+
   file_paths.each do |file_path|
     puts "Processing file: #{file_path}"
   end
@@ -34,6 +43,24 @@ def parse_options(argv)
   opt.on('-c') { |v| options[:c] = v }
   file_paths = opt.parse(argv)
   [options, file_paths]
+end
+
+def parse_ls_output(content)
+  lines = content.strip.split("\n")
+  file_paths = []
+
+  lines.each do |line|
+    next if line.start_with?('total')
+
+    parts = line.split
+    filename = parts[8]
+
+    filename = filename.split('->').first if filename.include?('->')
+
+    file_paths << filename unless filename == '.' || filename == '..'
+  end
+
+  file_paths
 end
 
 main
