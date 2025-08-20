1ca7719ef95f06d9427583d179ef31c7c69dadf0
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-08-20 23:20:39 +0900

    パイプで接続した際の処理をメソッド化、stats関連の変数名を修正

--- 05.wc/lib/wc.rb
+++ 05.wc/lib/wc.rb
@@ -7,18 +7,11 @@ def main
   options, file_paths = parse_options(ARGV)
 
   # パイプで接続した場合 $stdin.tty? は false になる
-  if file_paths.empty? && !$stdin.tty?
-    file_stats = count_content($stdin.read)
-    file_stats[:file_path] = nil
-    file_stats_list = [file_stats]
-    total_stats = nil
-  else
-    file_stats_list, total_stats = collect_file_stats(file_paths)
-  end
+  stats_list, total_stats = (file_paths.empty? && !$stdin.tty?) ? collect_stdin_stats : collect_file_stats(file_paths)
 
-  max_width = calculate_max_width(file_stats_list, total_stats)
+  max_width = calculate_max_width(stats_list, total_stats)
 
-  file_stats_list.each { |stats| output_format(stats, options, max_width) }
+  stats_list.each { |stats| output_format(stats, options, max_width) }
 
   output_format(total_stats, options, max_width) if total_stats
 end
@@ -41,8 +34,17 @@ def count_content(content)
   }
 end
 
+def collect_stdin_stats
+  stdin_stats = count_content($stdin.read)
+  stdin_stats[:file_path] = nil
+  stdin_stats_list = [stdin_stats]
+  total_stats = nil
+
+  [stdin_stats_list, total_stats]
+end
+
 def collect_file_stats(file_paths)
-  file_info_list = []
+  file_stats_list = []
   total_stats = { lines: 0, words: 0, bytes: 0, file_path: 'total' }
 
   file_paths.each do |file_path|
@@ -50,16 +52,16 @@ def collect_file_stats(file_paths)
     next unless file_stats
 
     file_stats[:file_path] = file_path
-    file_info_list << file_stats
+    file_stats_list << file_stats
 
     total_stats[:lines] += file_stats[:lines]
     total_stats[:words] += file_stats[:words]
     total_stats[:bytes] += file_stats[:bytes]
   end
 
-  total_stats = nil if file_info_list.size <= 1
+  total_stats = nil if file_stats_list.size <= 1
 
-  [file_info_list, total_stats]
+  [file_stats_list, total_stats]
 end
 
 def read_and_count_file(file_path)
@@ -75,25 +77,25 @@ def read_and_count_file(file_path)
   count_content(content)
 end
 
-def calculate_max_width(file_stats_list, total_stats)
-  return if file_stats_list.empty?
+def calculate_max_width(stats_list, total_stats)
+  return if stats_list.empty?
 
   # 表示されるかどうかにかかわらず、statsのうち最大幅となるものに合わせて右揃えする仕様にしている。
   # 3つのstatsのうちbytesが必ず最大幅となる。複数ファイルの場合はtotalのbytesが最大、単一ファイルの場合はそのファイルのbytesが最大となる。
   if total_stats
     total_stats[:bytes].to_s.length
   else
-    file_stats_list.first[:bytes].to_s.length
+    stats_list.first[:bytes].to_s.length
   end
 end
 
-def output_format(file_stats, options, max_width)
+def output_format(stats, options, max_width)
   output = %i[lines words bytes].filter_map do |key|
-    file_stats[key].to_s.rjust(max_width) if options[key] || options.empty?
+    stats[key].to_s.rjust(max_width) if options[key] || options.empty?
   end
 
-  # パイプで接続した場合、file_stats[:file_path] は nil になるため、file_pathは出力しない
-  output << file_stats[:file_path] if file_stats[:file_path]
+  # パイプで接続した場合、stats[:file_path] は nil になるため、file_pathは出力しない
+  output << stats[:file_path] if stats[:file_path]
   puts output.join(' ')
 end
 
