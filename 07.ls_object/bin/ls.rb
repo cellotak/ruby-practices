e5802a55c943b38555445b559dc293303e91262a
cellotak <tak.2cellos.ashi.777@gmail.com>
2026-01-15 23:59:26 +0900

    ls.rbの実装 (lオプションのみ対応で通常出力は仮実装)

---
+++ 07.ls_object/bin/ls.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../lib/options'
+require_relative '../lib/entry'
+require_relative '../lib/entry_list'
+require_relative '../lib/long_formatter'
+
+options = Options.new(ARGV)
+
+target_paths = options.paths.empty? ? ['.'] : options.paths
+
+target_files = []
+target_dirs = []
+
+sorted_paths = options.reverse? ? target_paths.sort.reverse : target_paths.sort
+
+sorted_paths.each do |path|
+  if File.file?(path)
+    target_files << path
+  elsif File.directory?(path)
+    target_dirs << path
+  elsif !File.exist?(path)
+    warn "ls: cannot access '#{path}': No such file or directory"
+  end
+end
+
+if target_files.any?
+  entries = target_files.map do |path|
+    Entry.new(File.dirname(path), File.basename(path))
+  end
+
+  list = EntryList.new(entries)
+
+  if options.long_format?
+    LongFormatter.new.format(list)
+  else
+    puts list.map(&:name).join('  ')
+  end
+end
+
+puts if !target_files.empty? && !target_dirs.empty?
+
+target_dirs.each_with_index do |dir_path, index|
+  puts "#{dir_path}:" if target_paths.size > 1
+
+  filenames = options.reverse? ? Dir.entries(dir_path).sort.reverse : Dir.entries(dir_path).sort
+
+  filenames.reject! { |name| name.start_with?('.') } unless options.all?
+
+  entries = filenames.map { |name| Entry.new(dir_path, name) }
+  list = EntryList.new(entries)
+
+  if options.long_format?
+    LongFormatter.new.format(list)
+  else
+    puts list.map(&:name).join('  ')
+  end
+
+  puts if index < target_dirs.size - 1
+end
---
+++ 07.ls_object/test/ls_test.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'fileutils'
+require 'open3'
+
+class LsCommandTest < Minitest::Test
+  # テスト用のサンドボックス（作業場）
+  SANDBOX_DIR = 'test/sandbox'
+
+  def setup
+    FileUtils.mkdir_p(SANDBOX_DIR)
+
+    FileUtils.touch("#{SANDBOX_DIR}/file_a.txt")
+    FileUtils.touch("#{SANDBOX_DIR}/file_b.txt")
+
+    FileUtils.touch("#{SANDBOX_DIR}/.hidden")
+
+    FileUtils.mkdir("#{SANDBOX_DIR}/subdir")
+    FileUtils.touch("#{SANDBOX_DIR}/subdir/subfile.txt")
+  end
+
+  def teardown
+    FileUtils.rm_rf(SANDBOX_DIR)
+  end
+
+  def run_ls(args = '')
+    `ruby bin/ls.rb #{args}`
+  end
+
+  def run_ls_stderr(args = '')
+    _out, err, _status = Open3.capture3("ruby bin/ls.rb #{args}")
+    err
+  end
+
+  def test_basic_listing
+    output = run_ls(SANDBOX_DIR)
+    assert_match(/file_a\.txt/, output)
+    assert_match(/file_b\.txt/, output)
+  end
+
+  def test_option_all
+    output_default = run_ls(SANDBOX_DIR)
+    refute_match(/\.hidden/, output_default)
+
+    output_all = run_ls("-a #{SANDBOX_DIR}")
+    assert_match(/\.hidden/, output_all)
+  end
+
+  def test_option_reverse
+    output_default = run_ls(SANDBOX_DIR)
+    assert output_default.index('file_a.txt') < output_default.index('file_b.txt'), 'Expected alphabetical order'
+
+    output_reverse = run_ls("-r #{SANDBOX_DIR}")
+    assert output_reverse.index('file_b.txt') < output_reverse.index('file_a.txt'), 'Expected reverse order'
+  end
+
+  def test_multiple_files
+    output = run_ls("#{SANDBOX_DIR}/file_a.txt #{SANDBOX_DIR}/file_b.txt")
+    assert_match(/file_a\.txt/, output)
+    assert_match(/file_b\.txt/, output)
+    refute_match(/subdir/, output)
+  end
+
+  def test_mixed_arguments
+    output = run_ls("#{SANDBOX_DIR}/subdir #{SANDBOX_DIR}/file_a.txt")
+
+    file_index = output.index('file_a.txt')
+    dir_content_index = output.index('subfile.txt')
+
+    assert file_index < dir_content_index, 'Files should be listed before directories'
+  end
+
+  def test_non_existent_file
+    err = run_ls_stderr('nothing_here.txt')
+    assert_match(/ls: cannot access 'nothing_here.txt': No such file or directory/, err)
+  end
+
+  def test_long_format_alignment
+    File.write("#{SANDBOX_DIR}/bigfile", 'a' * 1000)
+
+    output = run_ls("-l #{SANDBOX_DIR}/file_a.txt #{SANDBOX_DIR}/bigfile")
+
+    assert_match(/file_a\.txt/, output)
+    assert_match(/bigfile/, output)
+    assert_match(/total/, output)
+  end
+
+  def test_option_reverse_arguments
+    output_default = run_ls("#{SANDBOX_DIR}/file_a.txt #{SANDBOX_DIR}/file_b.txt")
+    assert output_default.index('file_a.txt') < output_default.index('file_b.txt')
+
+    output_reverse = run_ls("-r #{SANDBOX_DIR}/file_a.txt #{SANDBOX_DIR}/file_b.txt")
+    assert output_reverse.index('file_b.txt') < output_reverse.index('file_a.txt'), 'Arguments should be reversed'
+  end
+end
