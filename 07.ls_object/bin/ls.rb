f62c291623f6117449c5faa8738586dc8e9c98e7
cellotak <tak.2cellos.ashi.777@gmail.com>
2026-02-02 01:15:17 +0900

    ls.rbに直書きしていた処理をメインメソッド化

--- 07.ls_object/bin/ls.rb
+++ 07.ls_object/bin/ls.rb
@@ -7,41 +7,43 @@ require_relative '../lib/entry_list'
 require_relative '../lib/long_formatter'
 require_relative '../lib/default_formatter'
 
-options = Options.new(ARGV)
+def main
+  options = Options.new(ARGV)
+  formatter = options.long_format? ? LongFormatter.new : DefaultFormatter.new
+  paths = options.paths.empty? ? ['.'] : options.paths
 
-formatter = options.long_format? ? LongFormatter.new : DefaultFormatter.new
+  files = []
+  dirs = []
 
-paths = options.paths.empty? ? ['.'] : options.paths
+  sorted_paths = options.reverse? ? paths.sort.reverse : paths.sort
 
-files = []
-dirs = []
+  sorted_paths.each do |path|
+    if File.file?(path)
+      files << path
+    elsif File.directory?(path)
+      dirs << path
+    elsif !File.exist?(path)
+      warn "ls: cannot access '#{path}': No such file or directory"
+    end
+  end
 
-sorted_paths = options.reverse? ? paths.sort.reverse : paths.sort
+  if files.any?
+    list = EntryList.generate_from_files(files)
+    output = formatter.format(list)
+    puts output unless output.empty?
+  end
 
-sorted_paths.each do |path|
-  if File.file?(path)
-    files << path
-  elsif File.directory?(path)
-    dirs << path
-  elsif !File.exist?(path)
-    warn "ls: cannot access '#{path}': No such file or directory"
+  puts if !files.empty? && !dirs.empty?
+
+  dirs.each_with_index do |dir_path, index|
+    puts "#{dir_path}:" if paths.size > 1
+
+    list = EntryList.generate_from_directory(dir_path, options)
+    output = formatter.format(list)
+    puts output unless output.empty?
+
+    puts if index < dirs.size - 1
   end
 end
 
-if files.any?
-  list = EntryList.generate_from_files(files)
-  output = formatter.format(list)
-  puts output unless output.empty?
-end
-
-puts if !files.empty? && !dirs.empty?
-
-dirs.each_with_index do |dir_path, index|
-  puts "#{dir_path}:" if paths.size > 1
-
-  list = EntryList.generate_from_directory(dir_path, options)
-  output = formatter.format(list)
-  puts output unless output.empty?
-
-  puts if index < dirs.size - 1
-end
+main
